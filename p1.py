cc1e2857b034a8fd680f10c2114188dbd18e8a83
audiocx <67644946+audiocx@users.noreply.github.com>
2022-05-10 17:11:47 -0400

    Agregado el algoritmo recursivo
    Se agrego CDTI (contador de terreno inundado)

--- p1.py
+++ p1.py
@@ -1,5 +1,24 @@
 import sys
 
+def CDTI(m, c):
+    i, j = c
+
+    if i < 0 or i >= len(m) or j < 0 or j >= len(m[0]):
+        return 0
+    if m[i][j] == 'L':
+        return 0
+
+    suma = 1
+
+    for x in range(i - 1, i + 2):
+        for y in range(j - 1, j + 2):
+            if x >= 0 and x < len(m) and y >= 0 and y < len(m[0]):
+                if x != i or y != j:
+                    m[i][j] = 'L'
+                    suma += CDTI(m, (x, y))
+    return suma
+
+
 def main():
     # Extraemos la información de la entrada estandar, que en este caso es en forma de un archivo .bat
     data = sys.stdin.readlines()
@@ -7,7 +26,7 @@ def main():
     n_casos = int(data[0].strip('\n'))
 
     # Diccionario de tuplas, en donde en cada tupla se encuentra como (matriz nxm, [lista de tuplas de consultas ])
-    tuplas_matriz_consultas = {}
+    tuplas_matriz_consultas = []
 
     # Recorremos desde la linea 3 el documento de prueba, estableciendo la matriz y las consultas para cada caso
     # hasta que nos encontremos con un salto de linea
@@ -22,10 +41,13 @@ def main():
                 consultas.append(tuple(map(lambda x: int(x) - 1, data[linea].strip('\n').split(' '))))
             linea += 1
         linea += 1
-        tuplas_matriz_consultas[i + 1] = (matriz, consultas)
+        tuplas_matriz_consultas.append((matriz, consultas))
 
 
-    print(tuplas_matriz_consultas)
+    for matriz, consultas in tuplas_matriz_consultas:
+        for consulta in consultas:
+            print(CDTI(matriz, consulta))
+        print('\n')
 
 if __name__ == "__main__":
     main()
